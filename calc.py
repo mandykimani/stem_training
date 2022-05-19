186162314b64a8fc826bb72128c1163c85a58bb4
mandykimani <kimani.amanda.04@gmail.com>
2022-05-19 12:32:03 +0300

    strings in python

---
+++ calc.py
@@ -0,0 +1,5 @@
+f_num = input("Enter the first number :")
+s_num = input("Enter the second number :")
+sum_of_num = int(f_num) + int(s_num)
+print("The sum of the two numbers is") + str(sum_of_num)
+
---
+++ strings.py
@@ -0,0 +1,49 @@
+name = "Amanda Kimani"
+
+# Multiple line strings
+msg = """QRST126XDG Confirmed 
+        You have received 
+        ksh20000000 from you brother"""
+print(msg)  
+
+
+
+
+city = "nairobi"
+# .upper() to convert to upper case
+print(city.upper())
+
+uni = "JKUAT"
+print(uni.lower())
+
+fruit = "strawberry"
+
+print(fruit[:2])
+print(fruit[:5])
+print(fruit[5:])
+print(fruit[-3:])
+
+# strip() remove spaces before the words
+f_name = " Amanda"
+print (f_name.strip())
+
+# concatination
+f_name = "Amanda"
+s_name = "Kimani"
+full_name = f_name +s_name
+print(full_name)
+
+city = "London"
+print(len(city))
+
+word = "deoxyribonucleicacid"
+print(len(word))
+
+
+# int - > string
+x = 100 # inter
+y = 3.14 # float
+z = 1
+print(str(x))
+print(int(y))
+print(float(z))
